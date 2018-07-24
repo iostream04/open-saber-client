45d0a4aa0fe6a8ceb478a2980f871f99e9232a68
jyotsna <jyotsna.raveendran@tarento.com>
2018-07-24 16:42:06 +0530

    Issue #00: issue fix

--- src/main/java/io/opensaber/registry/client/OpensaberClient.java
+++ src/main/java/io/opensaber/registry/client/OpensaberClient.java
@@ -21,7 +21,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.message.BasicHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
